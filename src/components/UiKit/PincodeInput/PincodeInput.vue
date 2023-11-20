73f118827ad5d1d209f41aafc2120d3373b7a733
Valeriya <v.blinova@itsportal.ru>
2023-11-20 17:40:33 +0300

    text

--- src/components/UiKit/PincodeInput/PincodeInput.vue
+++ src/components/UiKit/PincodeInput/PincodeInput.vue
@@ -16,7 +16,7 @@
         no-error-icon
         :disable="disabled"
         :autofocus="i === 1"
-        type="number"
+        type="text"
         @update:model-value="nextInput"
         @click="onFocus(i)"
         @keydown.delete="userPin = []"
