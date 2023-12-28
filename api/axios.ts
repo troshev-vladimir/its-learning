064bc86204603ec0d07b80b9f8bd56723cdb7e53
Vladimir <v.troshev@itsportal.ru>
2023-12-28 15:42:02 +0300

    add lint

--- api/axios.ts
+++ api/axios.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios'
 
-const username = "REST_API";
-const password = "IPA_TSER";
+const username = 'REST_API'
+const password = 'IPA_TSER'
 // const authorizationBasic = window.btoa(username + ":" + password);
 
 const instance = axios.create({
-  baseURL: "https://max43.ru:12233/ka_uprbase2/ru_RU/hs/education/v1",
+  baseURL: 'https://max43.ru:12233/ka_uprbase2/ru_RU/hs/education/v1',
   // process.env.NODE_ENV === "development"
   //   ? "https://max43.ru:12244/ka_uprbase2/ru_RU/hs/education/v1"
   //   : "https://max43.ru:12233/ka_uprbase2/ru_RU/hs/education/v1",
@@ -17,39 +17,29 @@ const instance = axios.create({
     password: password,
   },
   headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json;charset=UTF-8",
-    Authorization: "Basic " + btoa(`${username}:${password}`),
+    Accept: 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8',
+    Authorization: 'Basic ' + btoa(`${username}:${password}`),
   },
-});
+})
 
 instance.interceptors.response.use(
   function (response) {
-    return response;
+    return response
   },
   function (error) {
     if (
-      process.env.NODE_ENV === "development" &&
-      process.env.FOR_PAGES !== "true"
+      process.env.NODE_ENV === 'development' &&
+      process.env.FOR_PAGES !== 'true'
     ) {
-      return Promise.reject("unauthorized");
+      return Promise.reject('unauthorized')
     }
 
     if (error.response && error.response.status === 401) {
-      window.dispatchEvent(
-        new CustomEvent("unauthorized", {
-          detail: {
-            message:
-              error.response.data ||
-              "Время действия токена истекло. Пожалуйста авторизуйтесь",
-          },
-        })
-      );
-
-      return { data: null };
+      return { data: null }
     }
-    return Promise.reject(error.response?.data || error.message);
+    return Promise.reject(error.response?.data || error.message)
   }
-);
+)
 
-export default instance;
+export default instance
--- api/candidate/index.ts
+++ api/candidate/index.ts
@@ -1,21 +1,14 @@
-import { type CandidateProgressResp, type CandidateProgressReq } from './types';
-import { 
-  type UserId, 
-  type Candidate 
-} from "~/api/candidate/types";
+import { type CandidateProgressResp, type CandidateProgressReq } from './types'
+import { type UserId, type Candidate } from '~/api/candidate/types'
 
-import axios from "../axios";
-const event = new CustomEvent("server-error");
-const eventWithPayload = (payload: object) => {
-  return new CustomEvent("server-error", { detail: payload });
-};
+import axios from '../axios'
 
 export interface candidateCreateResp {
-  id: string;
-  Name: string;
-  Confirm: boolean;
-  RegistrationDate: string;
-  alreadyExists: boolean;
+  id: string
+  Name: string
+  Confirm: boolean
+  RegistrationDate: string
+  alreadyExists: boolean
 }
 
 class CandidateMethods {
@@ -25,58 +18,58 @@ class CandidateMethods {
   ): Promise<candidateCreateResp> {
     return axios
       .post(
-        "candidatecreate",
+        'candidatecreate',
         JSON.stringify({
           id: phone,
           createnewpin: resend,
         })
       )
       .then((response) => {
-        return response.data[0];
+        return response.data[0]
       })
       .catch((error) => {
-        throw error;
-      });
+        throw error
+      })
   }
 
   сandidateConfirmation(id: UserId, pin: string): Promise<Candidate> {
     return axios
-      .put("candidateconfirmation", { id, pin })
+      .put('candidateconfirmation', { id, pin })
       .then((response) => {
-        return response.data[0];
+        return response.data[0]
       })
       .catch((error) => {
-        throw error;
-      });
+        throw error
+      })
   }
 
   candidateUpdate(user: Candidate) {
     return axios
-      .put("candidateupdate", user)
+      .put('candidateupdate', user)
       .then((response) => {
-        return response.data;
+        return response.data
       })
       .catch((error) => {
-        window.dispatchEvent(event);
-        throw error;
-      });
+        window.dispatchEvent(event)
+        throw error
+      })
   }
 
   candidateCurrentProgress(
     params: CandidateProgressReq
   ): Promise<CandidateProgressResp> {
     return axios
-      .get("candidatecurrentprogress", {
+      .get('candidatecurrentprogress', {
         params,
       })
       .then((response) => {
-        if (!response.data) return [];
-        return response.data[0];
+        if (!response.data) return []
+        return response.data[0]
       })
       .catch((error) => {
-        throw error;
-      });
+        throw error
+      })
   }
 }
 
-export default new CandidateMethods();
+export default new CandidateMethods()
