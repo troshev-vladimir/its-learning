4987e195c1ff31224617b483d2a1ed8ebce02d40
Vladimir <v.troshev@itsportal.ru>
2024-01-29 16:19:25 +0300

    add vk metric

--- server/plugins/extend-html.ts
+++ server/plugins/extend-html.ts
@@ -4,23 +4,13 @@ export default defineNitroPlugin((nitroApp) => {
     // console.log(html)
     html.head.push(
       `<!-- Yandex.Metrika counter -->
-      <script type="text/javascript" >
-         (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-         m[i].l=1*new Date();
-         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-         ym(96214885, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-         });
-      </script>
-      <noscript><div><img src="https://mc.yandex.ru/watch/96214885" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <script type="text/javascript" >  (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};  m[i].l=1*new Date();  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(96214885, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/96214885" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
       <!-- /Yandex.Metrika counter -->`
     )
+    html.head.push(
+      `<script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1844653-4Bofz"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1844653-4Bofz" style="position:fixed; left:-999px;" alt=""/></noscript>`
+    )
+
     // console.log(html)
   })
   // You can also intercept the response here.
