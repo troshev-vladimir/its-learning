1161221996dc103a20b59ca4839f9d6aa6f67ec6
Lisa <vliza162@gmail.com>
2024-01-30 14:17:51 +0300

    fixed popup

--- components/Ui/BasePopup/index.vue
+++ components/Ui/BasePopup/index.vue
@@ -1,104 +1,133 @@
 <template>
   <teleport to="#popups-container">
-    <dialog
-      ref="dialog"
-      :class="[$style.dialog, 'pretty-scroll']"
-      @click="close"
-    >
-      <div :class="[$style.content]" @click.stop>
+    <TransitionGroup name="popup" :duration="{ enter: 200, leave: 150 }">
+      <div
+        class="base-popup base-block"
+        v-if="modelValue"
+        :class="{ active: modelValue }"
+      >
         <client-only>
           <font-awesome-icon
             @click="close"
-            :class="$style.cancel"
+            class="cancel"
             icon="fa-solid fa-xmark"
-            class="text-primary"
           />
         </client-only>
-
-        <slot> </slot>
+        <div class="card pretty-scroll">
+          <slot></slot>
+        </div>
       </div>
-    </dialog>
+      <span
+        class="background"
+        v-if="hasBackground === true && modelValue"
+        @click="close"
+      ></span>
+    </TransitionGroup>
   </teleport>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
-  modelValue: boolean
-}>()
 const emit = defineEmits(['update:modelValue'])
+import { useBodyFreez } from '~/composables'
 
-const dialog = ref<HTMLDialogElement>()
-watch(
-  () => props.modelValue,
-  (value) => {
-    if (!dialog.value) return
+interface Props {
+  modelValue?: boolean
+  hasBackground?: boolean
+}
 
-    if (value) {
-      dialog.value.showModal()
-      document.body.classList.add('scroll-lock')
-    } else {
-      document.body.classList.remove('scroll-lock')
-      close()
-    }
-  }
-)
+const props = withDefaults(defineProps<Props>(), {
+  hasBackground: true,
+})
+const { modelValue } = toRefs(props)
+let { startBodyFreez } = useBodyFreez(modelValue)
 
 const close = () => {
-  if (!dialog.value) return
-  dialog.value.close()
   emit('update:modelValue', false)
 }
+
+onMounted(() => {
+  startBodyFreez()
+
+  document.body.addEventListener('keydown', function (e) {
+    if (e.key == 'Escape') {
+      close()
+    }
+  })
+})
+
+defineExpose({ close })
 </script>
 
-<style lang="scss" module>
-.dialog {
-  padding: 0;
-  border: none;
-  pointer-events: none;
-  opacity: 0;
-  transition: opacity 0.5s;
-  display: flex;
-  min-width: 600px;
+<style lang="scss" scoped>
+.base-popup {
+  display: none;
+  transition: 0.2s;
+}
 
-  &[open] {
-    opacity: 1;
-    pointer-events: inherit;
+.popup-enter-active {
+  animation: showPopup 0.2s ease;
+  @keyframes showPopup {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
-
-  &::backdrop {
-    background-color: rgba(0, 0, 0, 0.5);
-  }
-
-  .content {
-    position: relative;
-    width: 100%;
-  }
-
-  .cancel {
-    position: absolute;
-    top: 16px;
-    right: 16px;
-    font-size: $lg;
-    color: $white;
-    cursor: pointer;
+}
+.popup-leave-active {
+  animation: hidePopup 0.2s ease;
+  @keyframes hidePopup {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
   }
 }
 
 .base-popup.active {
-  animation: showPopup 0.2s ease;
+  display: block;
+  position: fixed;
+  z-index: 3;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+  max-width: 90vw;
+  max-height: 90vh;
+  padding: 0;
 }
 
-@keyframes showPopup {
-  from {
-    opacity: 0;
-    transform: scale(0.8);
-  }
-  to {
-    transform: scale(1);
-    opacity: 1;
-  }
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #00000056;
 }
-
+.card {
+  flex-direction: column;
+  max-width: 90vw;
+  max-height: 90vh;
+  width: 100%;
+  height: max-content;
+  height: inherit;
+  overflow: auto;
+}
+.cancel {
+  position: fixed;
+  z-index: 2;
+  top: 24px;
+  right: 24px;
+  font-size: $lg;
+  color: $gray-600;
+  cursor: pointer;
+}
 .cardContainer {
   flex: 1 1 auto;
   overflow: auto;
