1fb0df442ac0be46673dbbdf66d5c008961eea04
Vladimir <v.troshev@itsportal.ru>
2024-01-30 13:00:10 +0300

    change publick dir

---
+++ public/metrics/vk.js
@@ -0,0 +1,10 @@
+!(function () {
+  var t = document.createElement('script')
+  ;(t.type = 'text/javascript'),
+    (t.async = !0),
+    (t.src = 'https://vk.com/js/api/openapi.js?169'),
+    (t.onload = function () {
+      VK.Retargeting.Init('VK-RTRG-1844653-4Bofz'), VK.Retargeting.Hit()
+    }),
+    document.head.appendChild(t)
+})()
