19e778692cd22bf7f842513bb9de83b463c7cdf5
Lisa <vliza162@gmail.com>
2024-03-11 12:16:37 +0300

    fixed problems

--- features/Education/Lesson/Card/index.vue
+++ features/Education/Lesson/Card/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="fade" mode="out-in">
+  <transition name="fade" :duration="200">
     <div v-if="!isLoading" class="education-lesson-card">
       <div class="education-lesson-card__container">
         <div v-if="lesson?.videoLink" class="education-lesson-card__video">
@@ -71,8 +71,8 @@
         </UiBasePopup> -->
       </div>
     </div>
-    <Skeleton v-else />
   </transition>
+  <Skeleton v-show="isLoading" />
 </template>
 
 <script lang="ts" setup>
