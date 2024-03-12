5d214b96c593092b58b6703174ecf25f90df642f
Lisa <vliza162@gmail.com>
2024-03-12 09:44:17 +0300

    fix

--- features/Education/Lesson/Card/index.vue
+++ features/Education/Lesson/Card/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="fade" :duration="{ enter: 0, leave: 200 }">
+  <transition name="fade" mode="out-in" :duration="{ enter: 0, leave: 200 }">
     <div v-if="!isLoading" class="education-lesson-card">
       <div class="education-lesson-card__container">
         <div v-if="lesson?.videoLink" class="education-lesson-card__video">
